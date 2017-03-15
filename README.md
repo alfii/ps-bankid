51696e9f96b0a738ed1641c7719a69f743d40a8c
Andreas Kruger <ak@pasientsky.no>
2017-03-15 11:36:19 +0100

    Initial startup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ps-bankid
+
+Example Dockerfile:
+
+```
+FROM pasientskyhosting:ps-bankid:latest
+MAINTAINER Andreas Krüger <ak@patientsky.com>
+
+# Add bankid jar files to /data
+COPY src /data
+```
---
+++ scripts/start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Adaptions
+if [ -d "/adaptions" ]; then
+    # make scripts executable incase they aren't
+    chmod -Rf 750 /adaptions/*
+
+    # run scripts in number order
+    for i in `ls /adaptions/`; do /adaptions/$i ; done
+fi
+
+if [ ! -d /keystore ];
+then
+    echo "Keystore /keystore is not mounted with keys"
+    exit 1
+fi
+
+if [ ! -d /data ];
+then
+    echo "Missing /data directory with code to execute"
+    echo "Startup command is: command=java -cp lib/bcmail-jdk15-1.45.jar:lib/bcprov-jdk15-1.45.jar:CA\ Integration\ Server-jar-with-dependencies.jar melin.server.ServerHost"
+    exit 1
+fi
+
+# Start supervisord and services
+/usr/bin/supervisord -n -c /etc/supervisord.conf
